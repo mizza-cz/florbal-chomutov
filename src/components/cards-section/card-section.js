eb11fc0c63c94514c48b935335e18d02a33c8439
mizza-cz <mizza.cz666@gmail.com>
2023-10-12 10:15:33 +0200

    add masonry

--- src/components/cards-section/card-section.js
+++ src/components/cards-section/card-section.js
@@ -1,1 +1,5 @@
- 
+$(".card-columns").masonry({
+  itemSelector: ".card-col", // use a separate class for itemSelector, other than .col-
+  columnWidth: ".grid-sizer",
+  percentPosition: true,
+});
